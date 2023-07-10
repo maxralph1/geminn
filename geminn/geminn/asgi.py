79d79440042a18f648c002bd42535a4723020116
Max Ralph <maxralph01@gmail.com>
2023-07-10 16:46:30 +0400

    for production

--- geminn/geminn/asgi.py
+++ geminn/geminn/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gem-inn.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geminn.settings')
 
 application = get_asgi_application()
--- geminn/geminn/wsgi.py
+++ geminn/geminn/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gem-inn.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geminn.settings')
 
 application = get_wsgi_application()
